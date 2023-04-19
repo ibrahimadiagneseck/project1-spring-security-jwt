28a301deb4a86f5631d827a9c84aa76b38d195a3
Ibrahima Diagne Seck <120041057+ibrahimadiagneseck@users.noreply.github.com>
2023-04-19 19:59:47 +0000

    first commit

---
+++ src/main/java/sn/esp/project1springsecurity/Project1SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package sn.esp.project1springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1SpringSecurityApplication.class, args);
+	}
+
+}
